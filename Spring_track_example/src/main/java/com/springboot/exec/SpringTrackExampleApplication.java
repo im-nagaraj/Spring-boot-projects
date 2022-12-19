738baf2f23d11757604361f1ef0f62fc63c5a956
im-nagaraj <nagarajpkodli@gmail.com>
2022-12-19 18:36:29 +0530

    First commit

---
+++ Spring_track_example/src/main/java/com/springboot/exec/SpringTrackExampleApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.exec;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringTrackExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringTrackExampleApplication.class, args);
+	}
+
+}
